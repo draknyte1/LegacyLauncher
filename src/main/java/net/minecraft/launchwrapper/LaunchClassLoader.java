cbd3b5ba95fe175783535b487699717d299b1a6c
Alkalus <3060479+draknyte1@users.noreply.github.com>
2021-11-12 11:08:54 +0000

    Don't return if system classloader is null, Fixes debug saving not getting enabled if it system classloader is null.

--- src/main/java/net/minecraft/launchwrapper/LaunchClassLoader.java
+++ src/main/java/net/minecraft/launchwrapper/LaunchClassLoader.java
@@ -105,16 +105,14 @@ public class LaunchClassLoader extends URLClassLoader {
 		 * class path.
 		 */
 		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-		if (classLoader == null) {
-			return;
-		}
-
-		classLoader = classLoader.getParent(); // Launcher.ExtClassLoader
-		if (classLoader instanceof URLClassLoader) {
-			for (URL url : ((URLClassLoader) classLoader).getURLs()) {
-				addURL(url);
-			}
-		}
+		if (classLoader != null) {
+            classLoader = classLoader.getParent(); // Launcher.ExtClassLoader
+            if (classLoader instanceof URLClassLoader) {
+                for (URL url : ((URLClassLoader) classLoader).getURLs()) {
+                    addURL(url);
+                }
+            }
+        }
 
 		if (DEBUG_SAVE) {
 			int x = 1;
